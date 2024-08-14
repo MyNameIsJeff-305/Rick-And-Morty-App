9c062b370c98b468c1b49380595426f840bae68a
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-08-14 19:08:53 -0400

    Testing Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "./src/styles/global.css";`
+        }
+      }
+    }
+  }
 });
